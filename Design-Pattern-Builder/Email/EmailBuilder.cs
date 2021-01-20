b77f33464ef521c8baeb083e6236d0f27ed15b15
Nantharupan <nantharupan@users.noreply.github.com>
2021-01-20 21:30:26 +0100

    Builder Design pattern

---
+++ Design-Pattern-Builder/Email/EmailBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Pattern_Builder.Email
+{
+    public class EmailBuilder
+    {
+        private string ToEmailAddress;
+        private string Message;
+        private string FromEamilAddress;
+        private string Subject;
+        public EmailBuilder(string ToEmailAddress)
+        {
+            this.ToEmailAddress = ToEmailAddress;
+        }
+
+        public EmailBuilder AddMessage(string message)
+        {
+            this.Message = message;
+            return this;
+        }
+
+        public EmailBuilder AddFromEmail(string fromEmailAddress)
+        {
+            this.FromEamilAddress = fromEmailAddress;
+            return this;
+        }
+
+        public EmailBuilder AddSubject(string subject)
+        {
+            this.Subject = subject;
+            return this;
+        }
+
+        public void SendEmail()
+        {
+            Console.WriteLine(this.ToEmailAddress);
+            Console.WriteLine(this.FromEamilAddress);
+            Console.WriteLine(this.Message);
+            Console.WriteLine(this.Subject);
+        }
+
+
+    }
+}
---
+++ Design-Pattern-Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using Design_Pattern_Builder.Email;
+using System;
+
+namespace Design_Pattern_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var email = new EmailBuilder("to@emai.com")
+                .AddFromEmail("from@email.com")
+                .AddMessage("Test Messag")
+                .AddSubject("Test Subject");
+            email.SendEmail();
+
+            email = new EmailBuilder("to@emai.com")
+            .AddFromEmail("from@email.com")
+            
+            .AddSubject("Test Subject");
+            email.SendEmail();
+
+        }
+    }
+}
